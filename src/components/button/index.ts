01ac3da87c06ce2aed8140a9fb91379822c2ba8b
AndrewLevada <zipeofficial@gmail.com>
2021-03-02 20:45:43 +0300

    Card element added

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -7,12 +7,20 @@ export class ButtonElement extends LightCustomElement implements Button {
 	@property({type: String}) text : string = "";
 	@property({type: Boolean}) secondary : boolean = false;
 	@property({type: String}) icon : string | undefined = undefined;
+	@property({type: String}) link : string | undefined = undefined;
 
 	render() { return html`
-		<div class="button-wrapper ${this.secondary ? `button-secondary` : ``}">
-			${this.icon ? html`<span class="button-icon material-icons-round">${this.icon}</span>` : html``}
-			<span class="button-text">${this.text}</span>
-		</div>`;
+		${this.link ? html`
+			<a class="button-wrapper ${this.secondary ? `button-secondary` : ``}" href="${this.link}">
+				${this.icon ? html`<span class="button-icon material-icons-round">${this.icon}</span>` : html``}
+				<span class="button-text">${this.text}</span>
+			</a>
+		` : html`
+			<button class="button-wrapper ${this.secondary ? `button-secondary` : ``}">
+				${this.icon ? html`<span class="button-icon material-icons-round">${this.icon}</span>` : html``}
+				<span class="button-text">${this.text}</span>
+			</button>
+		`}`;
 	}
 
 	static get styles() {
@@ -24,4 +32,5 @@ export interface Button extends CustomElement {
 	text : string;
 	secondary: boolean;
 	icon? : string;
+	link? : string;
 }
--- src/pages/cv/script.ts
+++ src/pages/cv/script.ts
@@ -4,3 +4,4 @@ import "./styles.scss";
 // Components
 import "~c/header"
 import "~c/button"
+import "~c/card"
